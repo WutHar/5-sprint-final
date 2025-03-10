1ce2f3acf65283988065fe8600d807226f470260
WutHar <vikingcrewnord@gmail.com>
2025-03-10 18:56:07 +0400

    first commit

--- cmd/tracker/main.go
+++ cmd/tracker/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/Yandex-Practicum/go1fl-sprint5-final/internal/actioninfo"
-	"github.com/Yandex-Practicum/go1fl-sprint5-final/internal/daysteps"
-	"github.com/Yandex-Practicum/go1fl-sprint5-final/internal/personaldata"
-	"github.com/Yandex-Practicum/go1fl-sprint5-final/internal/trainings"
+	"5-sprint-final/internal/actioninfo"
+	"5-sprint-final/internal/daysteps"
+	"5-sprint-final/internal/personaldata"
+	"5-sprint-final/internal/trainings"
 )
 
 func main() {
