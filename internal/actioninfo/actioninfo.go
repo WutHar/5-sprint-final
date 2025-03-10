0f7d8db60c26807d67623ea638ed7d694d96ad92
WutHar <vikingcrewnord@gmail.com>
2025-03-10 19:12:02 +0400

    обновил пакеты

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,11 +1,26 @@
 package actioninfo
 
 import (
-	...
+	"fmt"
 )
 
-// создайте интерфейс DataParser
-...
+type DataParser interface {
+	Parse(datastring string) error
+	ActionInfo() (string, error)
+}
 
-// создайте функцию Info()
-...
+func Info(dataset []string, dp DataParser) {
+	for _, data := range dataset {
+		err := dp.Parse(data)
+		if err != nil {
+			fmt.Println("Ошибка парсинга:", err)
+			continue
+		}
+		info, err := dp.ActionInfo()
+		if err != nil {
+			fmt.Println("Ошибка получения информации:", err)
+			continue
+		}
+		fmt.Println(info)
+	}
+}
--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,19 +1,51 @@
 package daysteps
 
 import (
-	...
+	"5-sprint-final/internal/personaldata"
+	"5-sprint-final/internal/spentenergy"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
 )
 
 const (
 	StepLength = 0.65
 )
 
-// создайте структуру DaySteps
-...
+type DaySteps struct {
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
+}
 
+func (ds *DaySteps) Parse(datastring string) (err error) {
+	data := strings.Split(datastring, ",")
+	if len(data) != 2 {
+		return errors.New("неверный формат данных")
+	}
 
-// создайте метод Parse()
-...
+	steps, err := strconv.Atoi(data[0])
+	if err != nil {
+		return err
+	}
+	ds.Steps = steps
 
-// создайте метод ActionInfo()
-...
+	duration, err := time.ParseDuration(data[1])
+	if err != nil {
+		return err
+	}
+	ds.Duration = duration
+
+	return nil
+}
+func (ds DaySteps) ActionInfo() (string, error) {
+	if ds.Duration <= 0 {
+		return "", errors.New("продолжительность должна быть больше 0")
+	}
+	distance := float64(ds.Steps) * StepLength / 1000
+	calories := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", ds.Steps, distance, calories), nil
+}
